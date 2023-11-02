dcbf0d442a1642278e1d2615723e92560a49249c
Elsie Funkor <elsiefunk12@gmail.com>
2023-11-02 14:03:05 -0700

    update on 100 print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
-for i in range(ord('z'), ord('A') - 1, -1):
-    char = chr(i)
-    print(char, end='')
-
-print()  # Print a newline to end the line
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    print("{}".format(chr(i - 32) if i % 2 != 0 else chr(i)), end="")
