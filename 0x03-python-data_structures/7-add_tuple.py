20a77f62609495f3a4e2e2e77ea4df88dd61fd62
Elsie Funkor <elsiefunk12@gmail.com>
2023-11-05 14:04:44 -0800

    tuplse update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-def add_tuples(tuple_a=(), tuple_b=()):
-    # Initialize an empty list to store the result values
-    result = []
-
-    # Loop through indices 0 and 1 (for a 2-element tuple)
-    for i in range(2):
-        # If length of tuple_a is greater than the current index (i),
-        # take value from tuple_a at that index, otherwise, use 0.
-        a_value = tuple_a[i] if len(tuple_a) > i else 0
-
-        # If length of tuple_b is greater than the current index (i),
-        # take value from tuple_b at that index, otherwise, use 0.
-        b_value = tuple_b[i] if len(tuple_b) > i else 0
-
-        # Add values from tuple_a and tuple_b and append the result to the list
-        result.append(a_value + b_value)
-
-    # Convert the list of result values into a tuple and return it
-    return tuple(result)
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        a1 = 0
+        a2 = 0
+    if len(tuple_b) == 0:
+        b1 = 0
+        b2 = 0
+    if len(tuple_a) == 1:
+        a2 = 0
+        a1 = tuple_a[0]
+    if len(tuple_b) == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    if len(tuple_a) >= 2:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+    if len(tuple_b) >= 2:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+    return a1 + b1, a2 + b2
