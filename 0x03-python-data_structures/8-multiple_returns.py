14ff93c9ba7d65e41f0e911918e8bdfff87b7f9c
Elsie Funkor <elsiefunk12@gmail.com>
2023-11-05 14:06:49 -0800

    update task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-def add_tuples(tuple_a=(), tuple_b=()):
-    # Initialize an empty list to store the result values
-    result = []
-
-    # Loop through indices 0 and 1 (for a 2-element tuple)
-    for i in range(2):
-        # If length of tuple_a is greater than the current index (i),
-        # take value from tuple_a at that index, otherwise, use 0.
-        a_value = tuple_a[i] if len(tuple_a) > i else 0
-
-        # If length of tuple_b is greater than the current index (i),
-        # take value from tuple_b at that index, otherwise, use 0.
-        b_value = tuple_b[i] if len(tuple_b) > i else 0
-
-        # Add the values from tuple_a and tuple_b and append the result to the list
-        result.append(a_value + b_value)
-
-    return tuple(result)
+def multiple_returns(sentence):
+    a = len(sentence)
+    if a == 0:
+        b = None
+    else:
+        b = sentence[0]
+    return a, b
